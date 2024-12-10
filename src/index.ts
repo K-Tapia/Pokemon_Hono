73211152acaa4f16e13556a7d402d23c5d7c5d09
Kevin <kevintapia954@gmail.com>
2024-12-10 14:27:24 -0600

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-
+import { cors } from 'hono/cors'
+import PokeDB from "./PokemonStarterTempDB.json" with { type: "json" };
 const app = new Hono()
+
+app.use(cors())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.get('/api/pokemon', (c) => {
+  return c.json(PokeDB); // Send the imported JSON data as the response
+});
+
 
 const port = process.env.PORT || 10000
 console.log(`Server is running on http://localhost:${port}`)
