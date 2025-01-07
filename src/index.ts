e7f01a93e557c368e0b97a8089436e3c8cafeb91
Kevin <kevintapia954@gmail.com>
2025-01-07 11:01:33 -0600

    dynamic routing

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,44 @@ app.get('/api/pokemon', (c) => {
   return c.json(PokeDB); // Send the imported JSON data as the response
 });
 
+app.get('/api/pokemon/pokeName/Bulbasaur', (c) => {
+  const bulbasaur= PokeDB.pokemon.find(p=> p.pokeName==='Bulbasaur');
+  return c.json(bulbasaur); //Pull Data for mudkip
+});
+app.get('/api/pokemon/pokeName/Squirtle', (c) => {
+  const squirtle= PokeDB.pokemon.find(p=> p.pokeName==='Squirtle');
+  return c.json(squirtle); //Pull Data for mudkip
+});
+app.get('/api/pokemon/pokeName/Charmander', (c) => {
+  const charmander= PokeDB.pokemon.find(p=> p.pokeName==='Charmander');
+  return c.json(charmander); //Pull Data for mudkip
+});
+app.get('/api/pokemon/pokeName/Chikorita', (c) => {
+  const chikorita= PokeDB.pokemon.find(p=> p.pokeName==='Chikorita');
+  return c.json(chikorita); //Pull Data for mudkip
+});
+app.get('/api/pokemon/pokeName/Totodile', (c) => {
+  const totodile= PokeDB.pokemon.find(p=> p.pokeName==='Totodile');
+  return c.json(totodile); //Pull Data for mudkip
+});
+app.get('/api/pokemon/pokeName/Cyndaquil', (c) => {
+  const cyndaquil= PokeDB.pokemon.find(p=> p.pokeName==='Cyndaquil');
+  return c.json(cyndaquil); //Pull Data for mudkip
+});
+app.get('/api/pokemon/pokeName/Treecko', (c) => {
+  const treecko= PokeDB.pokemon.find(p=> p.pokeName==='Treecko');
+  return c.json(treecko); //Pull Data for mudkip
+});
+app.get('/api/pokemon/pokeName/Torchic', (c) => {
+  const torchic= PokeDB.pokemon.find(p=> p.pokeName==='Torchic');
+  return c.json(torchic); //Pull Data for mudkip
+});
+app.get('/api/pokemon/pokeName/Mudkip', (c) => {
+  const mudkip= PokeDB.pokemon.find(p=> p.pokeName==='Mudkip');
+  return c.json(mudkip); //Pull Data for mudkip
+});
+
+
 
 const port = process.env.PORT || 10000
 console.log(`Server is running on http://localhost:${port}`)
