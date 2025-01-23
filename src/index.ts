ef72238bc4ab690baeaf8d791304f535461370c7
Kevin <kevintapia954@gmail.com>
2025-01-23 11:55:34 -0600

    Added Regions to PokemonStarterDB

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,34 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-import PokeDB from "./PokemonStarterTempDB.json" with { type: "json" };
-const app = new Hono()
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import PokeDB from "./PokemonStarterTempDB.json";
+const app = new Hono();
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.get('/api/pokemon', (c) => {
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+app.get("/api/pokemon", (c) => {
   return c.json(PokeDB); // Send the imported JSON data as the response
 });
 
-app.get('/api/pokemon/pokeName/Bulbasaur', (c) => {
-  const bulbasaur= PokeDB.pokemon.find(p=> p.pokeName==='Bulbasaur');
-  return c.json(bulbasaur); //Pull Data for Bulbasaur
-});
-app.get('/api/pokemon/pokeName/Squirtle', (c) => {
-  const squirtle= PokeDB.pokemon.find(p=> p.pokeName==='Squirtle');
-  return c.json(squirtle); //Pull Data for Squirtle
-});
-app.get('/api/pokemon/pokeName/Charmander', (c) => {
-  const charmander= PokeDB.pokemon.find(p=> p.pokeName==='Charmander');
-  return c.json(charmander); //Pull Data for charmander
-});
-app.get('/api/pokemon/pokeName/Chikorita', (c) => {
-  const chikorita= PokeDB.pokemon.find(p=> p.pokeName==='Chikorita');
-  return c.json(chikorita); //Pull Data for chikorita
-});
-app.get('/api/pokemon/pokeName/Totodile', (c) => {
-  const totodile= PokeDB.pokemon.find(p=> p.pokeName==='Totodile');
-  return c.json(totodile); //Pull Data for totodile
-});
-app.get('/api/pokemon/pokeName/Cyndaquil', (c) => {
-  const cyndaquil= PokeDB.pokemon.find(p=> p.pokeName==='Cyndaquil');
-  return c.json(cyndaquil); //Pull Data for cyndaquil
-});
-app.get('/api/pokemon/pokeName/Treecko', (c) => {
-  const treecko= PokeDB.pokemon.find(p=> p.pokeName==='Treecko');
-  return c.json(treecko); //Pull Data for treecko
-});
-app.get('/api/pokemon/pokeName/Torchic', (c) => {
-  const torchic= PokeDB.pokemon.find(p=> p.pokeName==='Torchic');
-  return c.json(torchic); //Pull Data for torchic
-});
-app.get('/api/pokemon/pokeName/Mudkip', (c) => {
-  const mudkip= PokeDB.pokemon.find(p=> p.pokeName==='Mudkip');
-  return c.json(mudkip); //Pull Data for mudkip
+app.get("/api/pokemon/pokeName/:name", (c) => {
+  const name = c.req.param("name");
+  const pokemonData = PokeDB.pokemon.find((p) => p.pokeName === name);
+  return c.json(pokemonData); //Pull Data for mudkip
 });
 
+app.get("/api/pokemon/region/:region", (c) => {
+  const region = c.req.param("region");
+  const regionData = PokeDB.pokemon.filter((p) => p.region?.toLowerCase() === region?.toLowerCase());
+  return c.json(regionData);
+});
 
-
-const port = process.env.PORT || 10000
-console.log(`Server is running on http://localhost:${port}`)
+const port = process.env.PORT || 10000;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
