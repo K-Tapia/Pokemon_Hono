95935d37d67d867869cf4cbcc4ac06b81bcbf003
K-Tapia <kevintapia954@gmail.com>
2025-04-01 14:11:29 -0500

    backend modified

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.get("/api/pokemon", (c) => {
 
 app.get("/api/pokemon/pokeName/:name", (c) => {
   const name = c.req.param("name");
-  const pokemonData = PokeDB.pokemon.find((p) => p.pokeName === name);
+  const pokemonData = PokeDB.pokemon.find((p) => p.pokeName?.toLowerCase() === name?.toLowerCase());
   return c.json(pokemonData); //Pull Data for mudkip
 });
 
@@ -25,6 +25,42 @@ app.get("/api/pokemon/region/:region", (c) => {
   return c.json(regionData);
 });
 
+//use .include() since it checks to see if a specific type selected is inside of a substring with 2 typings
+// app.get("/api/pokemon/pokeType/:type", (c) => {
+//   const type = c.req.param("type")?.toLowerCase(); // Get the type from the URL
+
+//   const typeData = PokeDB.pokemon.filter((p) => 
+//     p.pokeType?.toLowerCase().includes(type)
+//   );
+
+//   return c.json(typeData);
+// });
+app.get("/api/pokemon/pokeType/:type", (c) => {
+  const type = c.req.param("type");
+  const typeData = PokeDB.pokemon.filter((p) => p.pokeType?.toLowerCase().includes(type?.toLowerCase()));
+  return c.json(typeData);
+});
+
+app.post("/api/pokemon/filter", async(c)=>{
+  const filter = await c.req.json()
+
+  if (!filter.kanto && !filter.johto && !filter.hoenn && !filter.fire && !filter.water &&!filter.grass) return c.json(PokeDB.pokemon)
+
+
+  const filteredPokemon = PokeDB.pokemon.filter((pokemon) => {
+    if (filter.kanto && pokemon.region.toLowerCase() === "kanto") return true
+    if(filter.johto && pokemon.region.toLowerCase() === 'johto') return true
+    if(filter.hoenn && pokemon.region.toLowerCase() === 'hoenn') return true
+    if (filter.fire && pokemon.pokeType.toLowerCase() === 'fire') return true
+    if (filter.water && pokemon.pokeType.toLowerCase() === "water") return true
+    if (filter.grass && pokemon.pokeType.toLowerCase() === "grass") return true
+    return false
+  })
+  
+  // console.log(body);
+return c.json(filteredPokemon)
+
+});
 const port = process.env.PORT || 10000;
 console.log(`Server is running on http://localhost:${port}`);
 
